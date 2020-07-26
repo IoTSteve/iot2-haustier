0e2ee626266f03931200f2a51d3795778e9e63bb
Steve <S_Reich.1@web.de>
2020-07-27 00:42:20 +0200

    first push of test git

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <RobotControlView>
+      <template v-slot:videocomponent>
+        <VideoPlayer></VideoPlayer>
+      </template>
+      <template v-slot:controlcomponent>
+        <Controls></Controls>
+      </template>
+    </RobotControlView>
+  </div>
+</template>
+
+<script>
+import RobotControlView from './views/RobotControlView.vue'
+import Controls from "./components/ControlsComponent";
+import VideoPlayer from "./components/VideoPlayerComponent";
+
+export default {
+  name: 'App',
+  components: {
+    RobotControlView,
+    VideoPlayer,
+    Controls,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
